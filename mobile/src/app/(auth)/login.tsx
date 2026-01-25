942ce2b1e7d1718e501f9e6f36e052486ead7f90
ntu254 <nttu254.vn@gmail.com>
2026-01-25 20:31:58 +0700

    fix: update structure file mobile

--- mobile/src/app/(auth)/login.tsx
+++
@@ -1,5 +0,0 @@
-import { LoginForm } from '@/features/auth/ui/LoginForm';
-
-export default function LoginScreen() {
-    return <LoginForm />;
-}
--- mobile/src/app/(auth)/sign-up.tsx
+++
@@ -1,5 +0,0 @@
-import { RegisterForm } from '@/features/auth/ui/RegisterForm';
-
-export default function SignUpScreen() {
-    return <RegisterForm />;
-}
